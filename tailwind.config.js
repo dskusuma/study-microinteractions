d7380b5997cd63051550e81012b6b464b21fca90
Dwi Satria (ibé) <dwisatriakusuma4@gmail.com>
2020-08-07 17:36:48 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/**
+ * Use this file to define custom styles.
+ * Reference: https://tailwindcss.com/docs/configuration/
+ */
+module.exports = {
+  important: true,
+  theme: {
+    extend: {},
+  },
+};
